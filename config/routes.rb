65df5cb4142abaa7f74a8adcab546fe7796cf1f0
joshualivesay <joshualivesay@gmail.com>
2016-12-22 19:30:25 -0500

    routes & gem corrections

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   resources :angels
   resources :landings
   root 'angels#index'
+
+
+  get '/about.html', to: 'templates#about'
+  get '/contact.html', to: 'templates#contact'
+  get '/events.html', to: 'templates#events'
+  get '/index.html', to: 'templates#index'
+  get '/menu.html', to: 'templates#menu'
+  get '/news.html', to: 'templates#news'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
