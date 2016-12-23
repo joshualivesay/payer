f65303577eac347bed574c318fdda276f77ace75
joshualivesay <joshualivesay@gmail.com>
2016-12-22 19:25:35 -0500

    Added Template

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,26 @@
+class TemplatesController < ApplicationController
+  def about
+
+  end
+
+  def contact
+
+  end
+
+  def index
+
+  end
+
+  def events
+
+  end
+
+  def menu
+
+  end
+
+  def news
+
+  end
+
+end
